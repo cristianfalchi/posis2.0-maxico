8a8d4e13cdfb58c04a813bf4505c5d6d1b886979
Cristian Falchi <cristianstafe@gmail.com>
2022-05-08 02:23:50 -0300

    actualizacion variables de entorno

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const config = {
     db: {
-        host: process.env.HOST || "localhost",
-        user: process.env.USER || "root",
+        host: process.env.HOST_NAME || "localhost",
+        user: process.env.USER_NAME || "root",
         password: process.env.PASSWORD || "root",
         database: process.env.DATABASE || "posis",
     }
